bafe3b3818f3471443288b59160592e27bd52b3c
Brandon Pham <pham-brandon@outlook.com>
2025-07-08 07:29:19 -0400

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+const App = () => {
+  return (
+    <div className='text-blue-500'>
+      <h1>Hello</h1>
+      <Button>Click here</Button>
+    </div>
+  )
+}
+
+export default App
